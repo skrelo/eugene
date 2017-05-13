97cb40dc8974b2b75c79e15d2fbabe51422b0a17
Scott Krelo <skrelo@gmail.com>
2017-05-12 19:11:38 -0600

    update to run in interval

--- index.js
+++ index.js
@@ -16,47 +16,55 @@ const transport = email.createTransport( {
 
 let runOnly = [];//['premier'];
 
-let listings = {};
-let activeSites = [];
-let siteRan = [];
 
-Promise.each( sites, ( site ) => {
-	let bypass = false;
-	let dontRun = false;
-	var nSite = require( `./sites/${site}` );
-	//let sName = site.replace(/\.js/, '');
-	let siteObj = new nSite();
-	if ( runOnly.length) {
-		siteObj.active = false;
-		if ( _.indexOf( runOnly, siteObj.source ) > -1 ) {
-			bypass = true;
+function Index() {
+	let listings = {};
+	let activeSites = [];
+	let siteRan = [];
+
+	Promise.each( sites, ( site ) => {
+		let bypass = false;
+		let dontRun = false;
+		var nSite = require( `./sites/${site}` );
+		//let sName = site.replace(/\.js/, '');
+		let siteObj = new nSite();
+		if ( runOnly.length ) {
+			siteObj.active = false;
+			if ( _.indexOf( runOnly, siteObj.source ) > -1 ) {
+				bypass = true;
+			}
 		}
-	}
-	if ( true === siteObj.active || true === bypass ) {
-		if ( _.indexOf(siteRan, siteObj.source) > -1) {
-			return true;
-		}else {
-			console.log( 'Running ' + siteObj.name );
-			return siteObj.run().then( ( list ) => {
-				//listings = listings.concat( listings, list );
-				listings = Object.assign( {}, listings, list);
-				//siteObj.done();
-				activeSites.push( siteObj );
-				siteRan.push( siteObj.source );
+		if ( true === siteObj.active || true === bypass ) {
+			if ( _.indexOf( siteRan, siteObj.source ) > -1 ) {
 				return true;
-			} );
+			} else {
+				console.log( 'Running ' + siteObj.name );
+				return siteObj.run().then( ( list ) => {
+					//listings = listings.concat( listings, list );
+					listings = Object.assign( {}, listings, list );
+					//siteObj.done();
+					activeSites.push( siteObj );
+					siteRan.push( siteObj.source );
+					return true;
+				} );
+			}
+		} else {
+			return true;
 		}
-	} else {
-		return true;
-	}
-} ).then( () => {
-	let len = Object.keys(listings).length;
-	console.log( `SENDING ${len} LISTINGS` );
-	if ( activeSites.length) {
-		const html = mail.format( listings );
-		mail.send( transport, html );
-	}
-	activeSites.forEach( (site ) => {
-		site.done();
-	});
-} );
+	} ).then( () => {
+		let len = Object.keys( listings ).length;
+		console.log( `SENDING ${len} LISTINGS` );
+		if ( len ) {
+			const html = mail.format( listings );
+			mail.send( transport, html );
+		}else {
+			console.log( 'NO NEW LISTINGS');
+
+		/*activeSites.forEach( ( site ) => {
+			site.done();
+		} );*/
+	} );
+
+}
+
+setInterval( Index, 300000);
